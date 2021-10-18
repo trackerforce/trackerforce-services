c0e2e6a9ccf1abd47730bdbb096f00631d4399ad
petruki <31597636+petruki@users.noreply.github.com>
2021-10-17 20:25:19 -0700

    Removed format from the scope

--- trackerforce-common-tenancy/src/main/java/com/trackerforce/common/tenant/model/TaskOption.java
+++ trackerforce-common-tenancy/src/main/java/com/trackerforce/common/tenant/model/TaskOption.java
@@ -7,8 +7,6 @@ import com.fasterxml.jackson.annotation.JsonInclude.Include;
 public class TaskOption {
 	
 	private String value;
-	
-	private String format;
 
 	public String getValue() {
 		return value;
@@ -16,14 +14,6 @@ public class TaskOption {
 
 	public void setValue(String value) {
 		this.value = value;
-	}
-
-	public String getFormat() {
-		return format;
-	}
-
-	public void setFormat(String format) {
-		this.format = format;
 	}
 
 }
