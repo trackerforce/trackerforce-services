75ab18f0bc54dcda7607979f80cad4df6c6cf28d
petruki <31597636+petruki@users.noreply.github.com>
2024-05-20 15:30:12 -0700

    Fixed embedded mongo dep for test

--- trackerforce-identity/src/main/java/com/trackerforce/identity/TrackerforceIdentityApplication.java
+++ trackerforce-identity/src/main/java/com/trackerforce/identity/TrackerforceIdentityApplication.java
@@ -1,21 +1,15 @@
 package com.trackerforce.identity;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = { "com.trackerforce" })
-public class TrackerforceIdentityApplication implements CommandLineRunner {
+public class TrackerforceIdentityApplication {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(TrackerforceIdentityApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-	
 	}
 	
 }
--- trackerforce-management/src/test/java/com/trackerforce/management/ApplicationTests.java
+++ trackerforce-management/src/test/java/com/trackerforce/management/ApplicationTests.java
@@ -1,13 +1,37 @@
 package com.trackerforce.management;
 
+import com.trackerforce.management.controller.*;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class ApplicationTests {
 
+	@Autowired
+	AgentController agentController;
+
+	@Autowired
+	GlobalController globalController;
+
+	@Autowired
+	ProcedureController procedureController;
+
+	@Autowired
+	TaskController taskController;
+
+	@Autowired
+	TemplateController templateController;
+
 	@Test
 	void contextLoads() {
+		assertNotNull(agentController);
+		assertNotNull(globalController);
+		assertNotNull(procedureController);
+		assertNotNull(taskController);
+		assertNotNull(templateController);
 	}
 
 }
